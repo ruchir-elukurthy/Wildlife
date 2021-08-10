7d90305bb1aabbd1480059bfff567564bbbeea8f
Ruchir <ruchir@pdx.edu>
2021-08-09 19:02:21 -0700

    Initial Commit

---
+++ Wildlife/WildlifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildlifeApp.swift
+//  Wildlife
+//
+//  Created by Ruchir on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct WildlifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
